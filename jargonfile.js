ace8f53b6e719a2d4501fcaa8e0ae5c57fbd4252
Amrit Panesar <me@amrit.pw>
2021-02-27 01:14:11 -0800

    more testing jargon

--- jargonfile.js
+++ jargonfile.js
@@ -1,3 +1,4 @@
 export default {
-    test: "This is a testing piece of jargon"
+    "test": "This is a testing piece of jargon",
+    "starsiege.exe": "The main starsiege executable a player runs to launch the game"
 }
